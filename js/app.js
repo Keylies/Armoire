4cba916f52b481af9cb7822f27a0541c065f823f
costajeanpierre <dasilva.j.pierre@gmail.com>
2016-02-11 19:32:03 +0100

    some new directives

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 var wardrobeApp = angular.module('wardrobeApp', []);
 
@@ -8,36 +8,42 @@ wardrobeApp.controller('mainController', ['$scope', function ($scope) {
     $scope.wardrobeWidth = 100;
     $scope.wardrobeHeight = 100;
     
+	/*
     $scope.checkOffsets = function (){
         console.log("bitch");
     }
-
+	*/
+/*
     $scope.addBlock = function () {        
         var li = document.createElement("li");
         li.setAttribute('generate-block', "");
         angular.element(document.getElementById('blocks-list')).append(li);
-    }
+    }*/
 }]);
+
 
 wardrobeApp.directive('generateSvg', function () {
     return {
         restrict: 'A',
         template: '<rect fill="rgb(0,0,255)">'
-    };
+    }
 });
 
-wardrobeApp.directive('generateBlockButton', function (){
+
+wardrobeApp.directive('generateblockbutton', function (){	
     return {
         restrict: 'E',
-        template: '<button generateLi>Ajouter un block</button>'
-    }; 
+        template: '<button generateli>Ajouter un block</button>'
+    }
 });
 
-wardrobeApp.directive('generateLi', function (){
-    return {
-        restrict: 'E',
-        template: '<li>Width : <input type="number">'
-    }; 
+wardrobeApp.directive('generateli', function ($compile){
+    return function (scope, element, attrs){
+		element.bind('click', function (){				
+			angular.element(document.getElementById('blocks-list')).append($compile('<li>Width : <input type="number">Height : <input type="number"></li>')(scope));			
+			scope.comteur++;
+		});	
+	};
 });
 
 // http://jsfiddle.net/ftfish/KyEr3/
