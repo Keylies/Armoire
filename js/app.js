013535700a096870078e51f8e358f6ff07bed555
Jean-Pierre Costa Da Silva <dasilva.j.pierre@gmail.com>
2016-02-11 17:25:41 +0100

    Osef

--- js/app.js
+++ js/app.js
@@ -2,14 +2,42 @@
 
 var wardrobeApp = angular.module('wardrobeApp', []);
 
-wardrobeApp.controller('blocksController', ['$scope', function ($scope) {
+wardrobeApp.controller('mainController', ['$scope', function ($scope) {
+    $scope.compteur = 0;
+    
+    $scope.wardrobeWidth = 100;
+    $scope.wardrobeHeight = 100;
+    
+    $scope.checkOffsets = function (){
+        console.log("bitch");
+    }
 
+    $scope.addBlock = function () {        
+        var li = document.createElement("li");
+        li.setAttribute('generate-block', "");
+        angular.element(document.getElementById('blocks-list')).append(li);
+    }
 }]);
 
 wardrobeApp.directive('generateSvg', function () {
+    return {
+        restrict: 'A',
+        template: '<rect fill="rgb(0,0,255)">'
+    };
+});
 
-	return {
-	    restrict: 'A',
-	    template: '<rect width="{{wardrobeWidth}}" height="{{wardrobeHeight}}" style="fill:rgb(0,0,255);">'
-	};
- });
+wardrobeApp.directive('generateBlockButton', function (){
+    return {
+        restrict: 'E',
+        template: '<button generateLi>Ajouter un block</button>'
+    }; 
+});
+
+wardrobeApp.directive('generateLi', function (){
+    return {
+        restrict: 'E',
+        template: '<li>Width : <input type="number">'
+    }; 
+});
+
+// http://jsfiddle.net/ftfish/KyEr3/
